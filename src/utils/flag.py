f24dfcc0e343ccea1081d9132ee12edf2fd43682
Tien Tran <tien.tran@sap.com>
2018-06-24 16:17:19 +0700

    Fix flag param

--- src/utils/flag.py
+++ src/utils/flag.py
@@ -7,7 +7,7 @@ FLAGS = flags.FLAGS
 # Define flags
 flags.DEFINE_string('output_dir', constant.LOCAL_OUTPUT, 'Output Directory.')
 flags.DEFINE_string('input_dir', constant.LOCAL_INPUT, 'Input Directory.')
-flags.DEFINE_integer('num_step', constant.DEFAULT_NUM_STEP, 'Number of training steps')
+flags.DEFINE_integer('num_steps', constant.DEFAULT_NUM_STEP, 'Number of training steps')
 flags.DEFINE_boolean('gs', False, 'Google Cloud Storage.')
 
 def get_flag_value(name):
