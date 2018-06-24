0f3300aba4e88513c7054ddfcd59b2bf339c59ea
Tien Tran <tien.tran@sap.com>
2018-06-24 17:34:10 +0700

    Revert

--- src/embedding/word_embedding.py
+++ src/embedding/word_embedding.py
@@ -9,9 +9,14 @@ import zipfile
 import datetime as dt
 import numpy as np
 
-# import local py files
-import embedding.flag as flag
-import embedding.constant as constant
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+# Define flags
+flags.DEFINE_string('output_dir', constant.LOCAL_OUTPUT, 'Output Directory.')
+flags.DEFINE_string('input_dir', constant.LOCAL_INPUT, 'Input Directory.')
+flags.DEFINE_integer('num_steps', constant.DEFAULT_NUM_STEP, 'Number of training steps')
+flags.DEFINE_boolean('gs', False, 'Google Cloud Storage.')
 
 def maybe_download(filename, url, expected_bytes):
     """Download a file if not present, and make sure it's the right size."""
@@ -32,7 +37,7 @@ def read_data(filename):
     """Extract the first file enclosed in a zip file as a list of words."""
     data = None
     data_bytes = None
-    if flag.check_flag_value(constant.FLAG_WORD_EMBEDDING_GS, True):
+    if FLAGS.gs == True:
       with tf.gfile.GFile(filename, "r") as f:
         data_bytes = f.read()
     else:
@@ -65,7 +70,7 @@ def build_dataset(words, n_words):
 def collect_data(vocabulary_size=10000):
   #  url = 'http://mattmahoney.net/dc/'
    # filename = maybe_download('text8.zip', url, 31344016)
-    input_path = os.path.join(flag.get_flag_value('input_dir'), "train.en")
+    input_path = os.path.join(FLAGS.input_dir, "train.en")
     vocabulary = read_data(input_path)
     print(vocabulary[:7])
     data, count, dictionary, reverse_dictionary = build_dataset(vocabulary,
@@ -158,7 +163,7 @@ with graph.as_default():
   init = tf.global_variables_initializer()
 
 def get_file_storage(output_path):
-  if flag.check_flag_value(flag.check_flag_value(constant.FLAG_WORD_EMBEDDING_GS), True):
+  if FLAGS.gs == True:
     return file_io.FileIO(output_path, 'w')
   return output_path
 
@@ -200,10 +205,10 @@ def run(graph, num_steps):
               log_str = '%s %s,' % (log_str, close_word)
             print(log_str)
       final_embeddings = normalized_embeddings.eval()
-      output_path = get_file_storage(os.path.join(flag.get_flag_value('output_dir'), "word_embeding_english.npy"))
+      output_path = get_file_storage(os.path.join(FLAGS.output_dir, "word_embeding_english.npy"))
       np.save(output_path, np.array(final_embeddings))
 
-num_steps = flag.get_flag_value('num_steps')
+num_steps = FLAGS.num_steps
 softmax_start_time = dt.datetime.now()
 run(graph, num_steps=num_steps)
 softmax_end_time = dt.datetime.now()
@@ -230,7 +235,7 @@ with graph.as_default():
     # Add variable initializer.
     init = tf.global_variables_initializer()
 
-num_steps = flag.get_flag_value('num_steps')
+num_steps = FLAGS.num_steps
 nce_start_time = dt.datetime.now()
 run(graph, num_steps)
 nce_end_time = dt.datetime.now()
